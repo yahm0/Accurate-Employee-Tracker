cf1bbde91a519838369d5f8737ef03d996a78a0f
yahm0 <stnt190@outlook.com>
2024-04-29 22:11:13 -0400

    made updats the amount of character the tables columns can hold.

--- db/schema.sql
+++ db/schema.sql
@@ -4,13 +4,13 @@
 -- Create a table for departments
 CREATE TABLE departments (
     department_id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL
+    name VARCHAR(30) NOT NULL
 );
 
 -- Create a table for roles
 CREATE TABLE roles (
     role_id INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(255) NOT NULL,
+    title VARCHAR(30) NOT NULL,
     salary DECIMAL(10, 2) NOT NULL,
     department_id INT,
     FOREIGN KEY (department_id) REFERENCES departments(department_id)
@@ -19,8 +19,8 @@ CREATE TABLE roles (
 -- Create a table for employees
 CREATE TABLE employees (
     employee_id INT AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(255) NOT NULL,
-    last_name VARCHAR(255) NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
     role_id INT,
     manager_id INT NULL,
     FOREIGN KEY (role_id) REFERENCES roles(role_id),
